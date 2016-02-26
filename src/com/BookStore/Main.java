787b6ca20850c17f4083779315799c4ab7cd027f
Victor Ursan <victor.ursan@gmail.com>
2016-02-26 17:05:56 +0200

    added src and test folder

---
+++ src/com/BookStore/Main.java
@@ -0,0 +1,11 @@
+package com.BookStore;
+
+/**
+ * Created by victor on 2/26/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
