4acfcfb6f097bfcf8015164b0340b5459925375a
Daniela Craciun <craciundaniela19@gmail.com>
2016-03-02 23:36:55 +0200

    Added Book and Client Repositories and generics to Repository interface

--- src/com/BookStore/Repository/IRepository.java
+++ src/com/BookStore/Repository/IRepository.java
@@ -2,10 +2,14 @@ package com.BookStore.Repository;
 
 import java.util.ArrayList;
 
-public interface IRepository {
-    void add(Object o);
-    Object get(Integer id);
-    ArrayList<Object> getall();
-    void update(Integer id, Object o);
-    void delete(Integer id);
+public interface IRepository<E> {
+    void add(E elem);
+
+    E get(int id);
+
+    ArrayList<E> getAll();
+
+    void update(int id, E elem);
+
+    void delete(int id);
 }
