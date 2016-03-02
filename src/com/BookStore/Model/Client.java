6f846939615918aa839499086938c16e81802f5a
Victor Ursan <victor.ursan@gmail.com>
2016-03-02 22:21:40 +0200

    changed Client

--- src/com/BookStore/Model/Client.java
+++ src/com/BookStore/Model/Client.java
@@ -1,14 +1,26 @@
 package com.BookStore.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
     private Integer id;
     private String firstName;
     private String lastName;
+    private List<Book> books;
 
     public Client(Integer id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.books = new ArrayList<>();
+    }
+
+    public Client(Integer id, String firstName, String lastName, List<Book> books) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
     }
 
     public Integer getId() {
@@ -29,5 +41,13 @@ public class Client {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 }
