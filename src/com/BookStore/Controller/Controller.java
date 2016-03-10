69fc660d9928c6c03728ae9122f1016c977e6de4
Victor Ursan <victor.ursan@gmail.com>
2016-03-10 21:41:04 +0200

    added addClient and addBook in Controller

--- src/com/BookStore/Controller/Controller.java
+++ src/com/BookStore/Controller/Controller.java
@@ -1,9 +1,13 @@
 package com.BookStore.Controller;
 
+import com.BookStore.Model.BaseEntity;
 import com.BookStore.Model.Book;
 import com.BookStore.Model.Client;
 import com.BookStore.Model.Validators.ValidatorException;
 import com.BookStore.Repository.IRepository;
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class Controller {
     private IRepository<Book> bookRepository;
@@ -15,14 +19,24 @@ public class Controller {
     }
 
     public void addBook(String title, String author, Long ISBN, String genre, String publisher, Integer price, Boolean available) throws ValidatorException {
-        //TODO
-//        Book book = new Book(1, title, author, ISBN, genre, publisher, price, available);
-//        bookRepository.add(book);
+        Stream<Book> books = StreamSupport.stream(bookRepository.getAll().spliterator(), false);
+        OptionalInt validID = books.mapToInt(BaseEntity::getId).max();
+        int id = 0;
+        if (validID.isPresent()) id = validID.getAsInt() + 1;
+        Book book = new Book(id, title, author, ISBN, genre, publisher, price, available);
+        bookRepository.add(book);
     }
 
     public void addClient(String firstName, String lastName) throws ValidatorException {
-        //TODO
-//        Client client = new Client(1, firstName, lastName);
-//        clientRepository.add(client);
+        Stream<Client> clients = StreamSupport.stream(clientRepository.getAll().spliterator(), false);
+        OptionalInt validID = clients.mapToInt(BaseEntity::getId).max();
+        int id = 0;
+        if (validID.isPresent()) id = validID.getAsInt() + 1;
+        Client client = new Client(id, firstName, lastName);
+        clientRepository.add(client);
+    }
+
+    public Iterable<Client> getAllClients() {
+        return clientRepository.getAll();
     }
 }
--- src/com/BookStore/Main.java
+++ src/com/BookStore/Main.java
@@ -1,7 +1,23 @@
 package com.BookStore;
 
+import com.BookStore.Controller.Controller;
+import com.BookStore.Model.Book;
+import com.BookStore.Model.Client;
+import com.BookStore.Model.Validators.BookValidator;
+import com.BookStore.Model.Validators.ClientValidator;
+import com.BookStore.Model.Validators.ValidatorException;
+import com.BookStore.Repository.InMemoryRepository;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Controller ctrl = new Controller(new InMemoryRepository<>(new BookValidator()), new InMemoryRepository<>(new ClientValidator()));
+        try {
+            ctrl.addClient("Ion", "Ion");
+            ctrl.addClient("Ion1", "Ion4");
+            ctrl.addClient("Ion2", "Ion3");
+            System.out.print(ctrl.getAllClients());
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+        }
     }
 }
