a766985949b4ba8c30a520c5f3f24aa74fc16428
Victor Ursan <victor.ursan@gmail.com>
2016-03-10 23:07:49 +0200

    refactored Controller

--- src/com/BookStore/Controller/Controller.java
+++ src/com/BookStore/Controller/Controller.java
@@ -18,21 +18,23 @@ public class Controller {
         this.clientRepository = clientRepository;
     }
 
-    public void addBook(String title, String author, Long ISBN, String genre, String publisher, Integer price, Boolean available) throws ValidatorException {
-        Stream<Book> books = StreamSupport.stream(bookRepository.getAll().spliterator(), false);
+    private int getValidIDForIteratory(Iterable<? extends BaseEntity<Integer>> iter) {
+        Stream<? extends BaseEntity<Integer>> books = StreamSupport.stream(iter.spliterator(), false);
         OptionalInt validID = books.mapToInt(BaseEntity::getId).max();
         int id = 0;
         if (validID.isPresent()) id = validID.getAsInt() + 1;
-        Book book = new Book(id, title, author, ISBN, genre, publisher, price, available);
+        return id;
+    }
+
+    public void addBook(String title, String author, Long ISBN, String genre, String publisher, Integer price, Boolean available) throws ValidatorException {
+        Integer validID = getValidIDForIteratory(bookRepository.getAll());
+        Book book = new Book(getValidIDForIteratory(bookRepository.getAll()), title, author, ISBN, genre, publisher, price, available);
         bookRepository.add(book);
     }
 
     public void addClient(String firstName, String lastName) throws ValidatorException {
-        Stream<Client> clients = StreamSupport.stream(clientRepository.getAll().spliterator(), false);
-        OptionalInt validID = clients.mapToInt(BaseEntity::getId).max();
-        int id = 0;
-        if (validID.isPresent()) id = validID.getAsInt() + 1;
-        Client client = new Client(id, firstName, lastName);
+        Integer validID = getValidIDForIteratory(clientRepository.getAll());
+        Client client = new Client(validID, firstName, lastName);
         clientRepository.add(client);
     }
 
