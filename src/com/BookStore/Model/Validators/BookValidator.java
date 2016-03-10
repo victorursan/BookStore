a82c8677b031994c4f3daa804ad573df42aa6e70
Victor Ursan <victor.ursan@gmail.com>
2016-03-10 20:19:28 +0200

    added IValidator interface and ValidatorException class

---
+++ src/com/BookStore/Model/Validators/BookValidator.java
@@ -0,0 +1,13 @@
+package com.BookStore.Model.Validators;
+
+import com.BookStore.Model.Book;
+
+/**
+ * Created by victor on 3/10/16.
+ */
+public class BookValidator implements IValidator<Book> {
+    @Override
+    public void validate(Book entity) throws ValidatorException {
+
+    }
+}
---
+++ src/com/BookStore/Model/Validators/ClientValidator.java
@@ -0,0 +1,13 @@
+package com.BookStore.Model.Validators;
+
+import com.BookStore.Model.Client;
+
+/**
+ * Created by victor on 3/10/16.
+ */
+public class ClientValidator implements IValidator<Client>{
+    @Override
+    public void validate(Client entity) throws ValidatorException {
+
+    }
+}
