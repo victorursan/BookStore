9b33fe9457f078f3f3ddab1af0ac0c3ff60a0505
Victor Ursan <victor.ursan@gmail.com>
2016-03-18 00:44:45 +0200

    added Back The FileRepository instances in Main

--- src/com/BookStore/Main.java
+++ src/com/BookStore/Main.java
@@ -1,10 +1,24 @@
 package com.BookStore;
+import com.BookStore.Controller.Controller;
+import com.BookStore.Model.Book;
+import com.BookStore.Model.Client;
+import com.BookStore.Model.Validators.BookValidator;
+import com.BookStore.Model.Validators.ClientValidator;
 import com.BookStore.Model.Validators.ValidatorException;
+import com.BookStore.Repository.BookFileRepository;
+import com.BookStore.Repository.ClientFileRepository;
+import com.BookStore.Repository.IRepository;
 import com.BookStore.View.Console;
 
 public class Main {
     public static void main(String[] args) throws ValidatorException {
-        Console console = new Console();
+        String bookPath = "./data/FileData/Books.txt";
+        String clientPath =  "./data/FileData/Clients.txt";
+        String purchasePath = "./data/FileData/Purchase.txt";
+
+        IRepository<Book> bookrepo = new BookFileRepository(new BookValidator(), bookPath);
+        IRepository<Client> clientrepo = new ClientFileRepository(new ClientValidator(), clientPath, purchasePath, bookrepo);
+        Console console = new Console(new Controller(bookrepo, clientrepo));
         console.run();
     }
 }
