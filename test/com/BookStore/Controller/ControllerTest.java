b199d35eb4c6ebfef9ef2a6f6740908f83306f2d
Victor Ursan <victor.ursan@gmail.com>
2016-03-18 15:18:02 +0200

    added some test to ControllerTest

--- test/com/BookStore/Controller/ControllerTest.java
+++ test/com/BookStore/Controller/ControllerTest.java
@@ -1,25 +1,50 @@
 package com.BookStore.Controller;
 
+import com.BookStore.Model.BaseEntity;
+import com.BookStore.Model.Book;
+import com.BookStore.Model.Client;
+import com.BookStore.Repository.IRepository;
+import com.BookStore.Repository.InMemoryRepository;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.junit.Assert.*;
 
 public class ControllerTest {
-
+    private Controller ctrl;
     @Before
     public void setUp() throws Exception {
-
+        IRepository<Book> bookrepo = new InMemoryRepository<>();
+        IRepository<Client> clientrepo = new InMemoryRepository<>();
+        ctrl = new Controller(bookrepo, clientrepo);
+        ctrl.addBook("LastBook", "Barry", 456789012L, "Fantasy", "SomePub", 10);
+        ctrl.addBook("SecondBook", "Jane", 234567890L, "Horror", "NicePub", 27);
+        ctrl.addBook("ThirdBook", "Foo", 345678900L, "SF", "AwesomePub", 7);
+        ctrl.addBook("Paradigme Universale", "Solomon Marcus", 9736975797L, "filosofie", "Paralela 45", 100);
+        ctrl.addClient("Laura", "Pop");
+        ctrl.addClient("Victor", "Ursan");
+        ctrl.buyBook(1,1);
+        ctrl.buyBook(0,0);
+        ctrl.buyBook(1,2);
+    }
+    private <T extends BaseEntity<Integer>> Stream<T> getStreamFromIterable(Iterable<T> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
     }
 
     @Test
     public void addBook() throws Exception {
-
+        ctrl.addBook("FirstBook", "John", 123456789L, "SF", "CoolPub", 17);
+        assertSame(getStreamFromIterable(ctrl.getAllBooks()).count(), 5L);
     }
 
     @Test
     public void addClient() throws Exception {
-
+        ctrl.addClient("Maria", "Ion");
+        assertSame(getStreamFromIterable(ctrl.getAllClients()).count(), 3L);
     }
 
     @Test
