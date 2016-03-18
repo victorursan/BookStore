fdcb0c0f5e8435eba07d8972e1af7250fc6d743d
Victor Ursan <victor.ursan@gmail.com>
2016-03-18 12:13:03 +0200

    added ControllerTest class

---
+++ test/com/BookStore/Controller/ControllerTest.java
@@ -0,0 +1,117 @@
+package com.BookStore.Controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by victor on 3/18/16.
+ */
+public class ControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void addBook() throws Exception {
+
+    }
+
+    @Test
+    public void addClient() throws Exception {
+
+    }
+
+    @Test
+    public void updateBook() throws Exception {
+
+    }
+
+    @Test
+    public void updateClient() throws Exception {
+
+    }
+
+    @Test
+    public void deleteBook() throws Exception {
+
+    }
+
+    @Test
+    public void deleteClient() throws Exception {
+
+    }
+
+    @Test
+    public void getAllClients() throws Exception {
+
+    }
+
+    @Test
+    public void getAllBooks() throws Exception {
+
+    }
+
+    @Test
+    public void availableBooks() throws Exception {
+
+    }
+
+    @Test
+    public void filterBooksByGenre() throws Exception {
+
+    }
+
+    @Test
+    public void filterBooksByAuthor() throws Exception {
+
+    }
+
+    @Test
+    public void filterBooksCheaperThan() throws Exception {
+
+    }
+
+    @Test
+    public void filterBooksMoreExpensiveThan() throws Exception {
+
+    }
+
+    @Test
+    public void clientWhoSpentMost() throws Exception {
+
+    }
+
+    @Test
+    public void clientWithMostBooks() throws Exception {
+
+    }
+
+    @Test
+    public void buyBook() throws Exception {
+
+    }
+
+    @Test
+    public void returnBook() throws Exception {
+
+    }
+
+    @Test
+    public void clientBooks() throws Exception {
+
+    }
+
+    @Test
+    public void booksWithISBN() throws Exception {
+
+    }
+
+    @Test
+    public void uniqueAvailableBooks() throws Exception {
+
+    }
+}
